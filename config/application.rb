0551cc22e87e23083fd7d1ee683e9425fb02955d
root <root@localhost>
2025-09-17 23:05:04 +0000

    initial test commit

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,7 @@ module BookCollectionSolution
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_051503) do
-
+ActiveRecord::Schema[8.0].define(version: 2021_09_28_051503) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.decimal "price"
     t.date "published_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
 end
